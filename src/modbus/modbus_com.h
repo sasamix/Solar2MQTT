d6044b735db078599bab4d44a0db2561698d2228
sasamix <asm@pioner.kz>
2026-09-23 14:11:18 +0500

    Fix const qualifier for Modbus result text

--- src/modbus/modbus_com.h
+++ src/modbus/modbus_com.h
@@ -38,7 +38,7 @@ public:
 private:
     String toBinary(uint16_t input);
     bool decodeDiematicDecimal(uint16_t int_input, int8_t decimals, float *value_ptr);
-    const char *getModbusResultText(uint8_t result);
+    const char *getModbusResultText(uint8_t result) const;
     bool logModbusResult(uint8_t result, uint16_t startRegister, uint16_t registerCount);
     bool getModbusValue(uint16_t register_id,
                         modbus_entity_t modbus_entity,
