63408545610d68a7abbeae4a643b37333df1b97c
sasamix <asm@pioner.kz>
2026-09-25 03:39:00 +0500

    Expand PowMr watch register coverage

--- src/modbus/modbus.h
+++ src/modbus/modbus.h
@@ -119,7 +119,7 @@ private:
     uint8_t _powmrSocHistoryCount = 0;
     unsigned long _powmrSocLastSampleMs = 0;
 
-    static constexpr uint8_t kPowMrWatchCount = 27;
+    static constexpr uint8_t kPowMrWatchCount = 46;
     uint16_t _powmrWatchValues[kPowMrWatchCount] = {};
     bool _powmrWatchValid = false;
     uint32_t _powmrWatchCapturedAt = 0;
